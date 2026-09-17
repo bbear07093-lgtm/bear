233812126c203cb791fefbcc9628150018e31d7d
bbear07093-lgtm <bbear07093@gmail.com>
2026-09-17 13:44:08 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8729235570:AAHtlsdBhenTt6CYepGxQtjg7Z5gDKaFoEw")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "2010030869"))
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://bear_qo11_user:xiRoIfNTNjih3bnLQCvn3xNxz9rbNUj8@dpg-dalqg72d0e5s738a15ug-a.oregon-postgres.render.com/bear_qo11")
+
+# Telethon uchun
+API_ID = int(os.getenv("API_ID", "38486800"))
+API_HASH = os.getenv("API_HASH", "c5fc7e4d2190b89e5ce8ea01c0369f09")
